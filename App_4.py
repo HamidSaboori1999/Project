ab56863ddd522d65b0bf76d2383238b9bc8d8dd7
Hamid Saboori <hamid1381saboori@yahoo.com>
2023-08-05 19:13:58 +0430

    Application for detecting the largest and smallest number in the list
    Application for detecting the largest and smallest number in the list

---
+++ App_4.py
@@ -0,0 +1,8 @@
+my_number_list = [12, 64, 23, 98, 32, 5]
+
+my_number_list.sort()
+
+min_number = my_number_list[0]
+max_number = my_number_list[-1]
+
+print(f"Max Number = {max_number}\nMin Number = {min_number}")
